6fa15f9da3a3df7771d3b4095e2fae442459125f
Falade Joseph <faladejoseph400@gmail.com>
2025-10-23 11:42:48 +0100

    Create add to library function

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+    if (!new.target) {
+        throw Error("You must use new operator")
+    }
+
+    this.book = title;
+    this.author = author;
+    this.size = pages;
+    this.read = status;
+    this.info = function () {
+        return ` ${this.book} by ${this.author}, ${this.size}, ${this.read}`
+    };
+}
+
+function addBookToLibrary(title, author, pages, status) {
+    const newBook = new Book(title, author, pages, status);
+    myLibrary.push(newBook);
+    return newBook;
+}
+
+const book1 = addBookToLibrary()
+
+// const book1 = new Book("Macbeth", "Shakespeare", "60 pages", "read");
+// console.log(book1.info());
+// this.book1.push = myLibrary;
